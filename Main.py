ca4123accb37911e386f5dd4f92c7dc4404bbfa3
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-01-12 13:18:45 -0800

    Enemy completed.

--- Main.py
+++ Main.py
@@ -1,3 +1,6 @@
+from Enemy import * 
 
+enemy = Enemy()
+enemy.type_of_enemy = 'Grunt'
 
-print(health_points)
+print(f'{enemy.type_of_enemy} has {enemy.health_points} health points and can do an attack of {enemy.attack_damage}')

42fff768810e65751e613d58544b308af2da1a49
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-01-14 12:44:04 -0800

    Applying Polymorphism in Python Arena

--- Main.py
+++ Main.py
@@ -2,11 +2,14 @@ from Enemy import *
 from Draugr import *
 from Giant import *
 
+
+def battle(e: Enemy):
+    e.talk()
+    e.attack()
+
+
 Draugr = Draugr(10, 1)
 Giant = Giant(50, 10)
 
-print(f'{Draugr.get_type_of_enemy()} has {Draugr.health_points} health points and can do attack of {Draugr.attack_damage}')
-print(f'{Giant.get_type_of_enemy()} has {Giant.health_points} health points and can do attack of {Giant.attack_damage}')
-
-Draugr.talk()
-Giant.talk()
+battle(Draugr)
+battle(Giant)

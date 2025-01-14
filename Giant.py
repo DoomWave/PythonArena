d0fe063232680805f5251828d8be8b1a8f00c497
Najair Acuna <123853739+DoomWave@users.noreply.github.com>
2025-01-13 16:01:35 -0800

    new enemy added, and more interactions added. Inheritance complete it.

--- Giant.py
+++ Giant.py
@@ -1,15 +1,12 @@
 from Enemy import *
 
 
-class Draugr(Enemy):
+class Giant(Enemy):
 
     def __init__(self, health_points, attack_damage):
-        super().__init__(type_of_enemy='Draugr',
+        super().__init__(type_of_enemy='Giant',
                          health_points=health_points,
                          attack_damage=attack_damage)
 
     def talk(self):
-        print('*Grumbling in Nordic Language*')
-
-    def battle_cry(self):
-        print('The Draugr is screaming his battle cry')
+        print('Giant is looking you without making any noise')
--- Main.py
+++ Main.py
@@ -1,6 +1,12 @@
 from Enemy import *
 from Draugr import *
+from Giant import *
 
-Draugr = Enemy('Draugr', 10, 1)
+Draugr = Draugr(10, 1)
+Giant = Giant(50, 10)
 
-print(Draugr.get_type_of_enemy())
+print(f'{Draugr.get_type_of_enemy()} has {Draugr.health_points} health points and can do attack of {Draugr.attack_damage}')
+print(f'{Giant.get_type_of_enemy()} has {Giant.health_points} health points and can do attack of {Giant.attack_damage}')
+
+Draugr.talk()
+Giant.talk()
